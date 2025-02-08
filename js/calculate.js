fc700268c8bea21f32f7b2d1b3d3abdcb543e447
nahidul <nahidulisalm13@gmai.com>
2025-02-09 00:52:35 +0600

    add all calculate function

--- js/calculate.js
+++ js/calculate.js
@@ -1,5 +1,5 @@
+//1.triangle calculate---------------------------------------------------------
 function triangleCalculate() {
-  //triangle calculate---------------------------------------------------------
   //width
   const w = document.getElementById("triangleWidth");
   const inputWidth = w.value;
@@ -16,7 +16,7 @@ function triangleCalculate() {
   console.log(area);
 }
 
-//Trianlge calculate------------------------------------------------------
+//2.Trianlge calculate------------------------------------------------------
 function rectangleCalculate() {
   //width
   const widthInput = document.getElementById("rectangleWidth");
@@ -30,5 +30,71 @@ function rectangleCalculate() {
   const l = parseFloat(valueLength);
   console.log(l);
   const area = w * l;
+  console.log(area);
+}
+
+// 3.parallelogram calculate ----------------------------------------------
+function parallelogramCalculate() {
+  //width
+  const widthInput = document.getElementById("parallelogramWidth");
+  const winp = widthInput.value;
+  const b = parseFloat(winp);
+  console.log(b);
+
+  //length
+  const lengthInput = document.getElementById("parallelogramHeight");
+  const valueLength = lengthInput.value;
+  const h = parseFloat(valueLength);
+  console.log(h);
+  const area = b * h;
+  console.log(area);
+}
+
+//4.rhumbus-------------------------------------------------
+function rhombusCalculate() {
+  //width
+  const widthInput = document.getElementById("rhombusd1");
+  const winp = widthInput.value;
+  const b = parseFloat(winp);
+  console.log(b);
+
+  //length
+  const lengthInput = document.getElementById("rhombusd2");
+  const valueLength = lengthInput.value;
+  const h = parseFloat(valueLength);
+  console.log(h);
+  const area = 0.5 * b * h;
+  console.log(area);
+}
+
+//5. pentagon Calculate--------------------------------------------------------
+function pentagonCalculate() {
+  const widthInput = document.getElementById("pentagonb");
+  const winp = widthInput.value;
+  const b = parseFloat(winp);
+  console.log(b);
+
+  //length
+  const lengthInput = document.getElementById("pentagonh");
+  const valueLength = lengthInput.value;
+  const h = parseFloat(valueLength);
+  console.log(h);
+  const area = 0.5 * b * h;
+  console.log(area);
+}
+
+//ellipse Calculate----------------------------------------------------------
+function ellipseCalculate() {
+  const widthInput = document.getElementById("ellipseA");
+  const winp = widthInput.value;
+  const b = parseFloat(winp);
+  console.log(b);
+
+  //length
+  const lengthInput = document.getElementById("ellipseB");
+  const valueLength = lengthInput.value;
+  const h = parseFloat(valueLength);
+  console.log(h);
+  const area = 3.1416 * b * h;
   console.log(area);
 }
